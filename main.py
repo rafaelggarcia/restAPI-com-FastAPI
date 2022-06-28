a523a29a026fa652be78efe06d0be14d8751dfe5
RafaelGarcia <rafael_ggarcia@outlook.com>
2022-06-28 16:25:43 -0300

    criadas as funções para trazer o ranking da competição e para encerrar a competição.

--- main.py
+++ main.py
@@ -56,3 +56,47 @@ async def inserir_dados(competicao: Competicao):
                     VALUES ('{competicao.competicao}','{competicao.atleta}', {competicao.value}, '{competicao.unidade}')""")
     db.commit()
     return {"message": "Dados inseridos com sucesso"}
+
+
+@app.get("/ranking/{competicao}")
+async def ranking(competicao: str):
+    if competicao == "corrida100m":
+        dados = conn.execute( """SELECT atleta,                                
+                                           RANK() OVER (ORDER BY value ASC) AS posicao_ranking, 
+                                           value
+                                      FROM resultados_competicoes
+                                      WHERE competicao = ?""", (competicao,) )
+    elif competicao == "dardo":
+        dados = conn.execute("""SELECT atleta,                                
+                                           RANK() OVER (ORDER BY value DESC) AS posicao_ranking,
+                                           value
+                                      FROM resultados_competicoes
+                                      WHERE competicao = ?""", (competicao,) )
+    else:
+        dados = conn.execute( """SELECT atleta,                                
+                                                   RANK() OVER (ORDER BY value DESC) AS posicao_ranking,
+                                                   value
+                                              FROM resultados_competicoes
+                                              WHERE competicao = ?""", (competicao,))
+    dados = dados.fetchall()
+    dict_dados = {}
+    for dado in dados:
+        dict_dados[dado[0]] = {"ranking": dado[1], "value": dado[2]}
+    return dict_dados
+
+
+@app.post("/fechar_competicao")
+async def fechar_competicao(nome_competicao: str):
+    dados = conn.execute( """SELECT COUNT (competicao)                            
+
+                                      FROM competicoes
+                                      WHERE competicao = ?""", (nome_competicao,) )
+    contagem = dados.fetchall()[0][0]
+    if contagem == 1:
+        conn.execute(""" UPDATE competicoes
+        SET 
+        status = 1
+        WHERE competicao = ?""", (nome_competicao,) )
+        db.commit()
+        return {"message": "Competição finalizada com sucesso!"}
+    return {"message": "Essa competição ainda não foi aberta! Caso queira abrir uma nova competição utilize a rota /criar_competicao"}
