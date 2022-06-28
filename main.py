4aa73b8664af9b768a95a61c2b6e906af2a5b7e3
RafaelGarcia <rafael_ggarcia@outlook.com>
2022-06-28 16:20:49 -0300

    função para inserir os dados

--- main.py
+++ main.py
@@ -34,3 +34,25 @@ async def criar_competicao(nome_competicao: str):
         return {"message": "Competição criada com sucesso"}
 
     return {"message": "Essa competição já existe!"}
+
+
+@app.post("/inserir_dados")
+async def inserir_dados(competicao: Competicao):
+    if isinstance(competicao.value, float):
+        competicao.value = competicao.value
+    else:
+        competicao.value = max(competicao.value)
+    nome_competicao = competicao.competicao
+    dados = conn.execute("""SELECT status from competicoes WHERE  competicao = ?""", (nome_competicao,) )
+    print(dados)
+    dados = dados.fetchall()
+    print(dados)
+    if len(dados) == 0:
+        return {"message": "Essa competição ainda não foi aberta! Caso queira abrir uma nova competição utilize a rota /criar_competicao"}
+    if dados[0][0] == 1:
+        return {"message": "Essa competição já foi fechada!"}
+    conn.execute(f"""INSERT
+                     INTO resultados_competicoes (competicao, atleta, value, unidade)
+                    VALUES ('{competicao.competicao}','{competicao.atleta}', {competicao.value}, '{competicao.unidade}')""")
+    db.commit()
+    return {"message": "Dados inseridos com sucesso"}
